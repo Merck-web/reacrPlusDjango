b88298a94d7d55f0cd85e301958089708386624b
Your Name <you@example.com>
2022-03-12 12:55:44 +0500

    serializer from rest_fraemvork

--- front/src/screens/HomeScreen.js
+++ front/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, CardGroup } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import axios from "axios";
 
@@ -17,7 +17,6 @@ function HomeScreen() {
   return (
     <div>
       <h1>Last Production</h1>
-      <CardGroup>
         <Row>
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -26,7 +25,6 @@ function HomeScreen() {
             </Col>
           ))}
         </Row>
-      </CardGroup>
     </div>
   );
 }
