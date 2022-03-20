a6b87db58b78964a52fa07a4b301696722c666bb
Your Name <you@example.com>
2022-03-20 12:53:16 +0500

    back auth, 50%

--- back/base/views.py
+++ back/base/views.py
@@ -3,13 +3,33 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Product
 from .products import products
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 # Create your views here.
 
-@api_view(['GET'])
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["GET"])
 def getRoutes(request):
     routes = [
         "/api/products/",
@@ -23,13 +43,22 @@ def getRoutes(request):
     ]
     return Response(routes)
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)    
+
+@api_view(["GET"])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
 def getProduct(request, pk):
     # product = None
     # for i in products:
@@ -38,6 +67,5 @@ def getProduct(request, pk):
     #         break
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
-        
-    return Response(serializer.data)
 
+    return Response(serializer.data)
