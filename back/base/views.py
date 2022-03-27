b69edeee542e972f9be974223d56395d77b4b685
Your Name <you@example.com>
2022-03-27 12:28:14 +0500

    Список пользователей защита роутов на бэке и регистрация

--- back/base/views.py
+++ back/base/views.py
@@ -1,17 +1,22 @@
+from email import message
 from itertools import product
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 # Create your views here.
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -27,6 +32,24 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["POST"])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data["name"],
+            username=data["email"],
+            email=data["email"],
+            password=make_password(data["password"]),
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {"detail": "User with this email already exists"}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
@@ -45,9 +68,18 @@ def getRoutes(request):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
